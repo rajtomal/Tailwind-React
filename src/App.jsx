4499f6848a6d6fae7eb09537a43a7e573abe0ad3
Tomal Ahmed Raj <sj.tomalahmedraj@gmail.com>
2023-10-23 18:24:26 +0600

    mmmmm

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { Button } from 'flowbite-react';
+import NavBar from './Navbar/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-[44px] font-bold underline">
-        Hello world!
-      </h1>
-      <Button>Click me</Button>
+      <NavBar></NavBar>
     </>
   )
 }
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Button, Navbar } from 'flowbite-react';
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+
+function NavBar() {
+    return (
+        <>
+            <Navbar fluid rounded>
+                    <Navbar.Toggle />
+                <Navbar.Brand href="#">
+                    <img src="https://www.flowbite-react.com/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">React</span>
+                </Navbar.Brand>
+                <div className="flex md:order-2">
+                    <Button>Get started</Button>
+                </div>
+                <Navbar.Collapse>
+                    <Navbar.Link href="#" active>
+                        Home
+                    </Navbar.Link>
+                    <Navbar.Link href="#">About</Navbar.Link>
+                    <Navbar.Link href="#">Services</Navbar.Link>
+                    <Navbar.Link href="#">Pricing</Navbar.Link>
+                    <Navbar.Link href="#">Contact</Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    )
 }
 
-export default Navbar
+
+export default NavBar
