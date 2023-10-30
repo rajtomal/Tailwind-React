3b7eecc7602e7d2909fe3098865d4dd3445594e2
Tomal Ahmed <sj.tomalahmedraj@gmail.com>
2023-10-30 21:14:04 +0600

    hhhhh

--- src/Banner/BannerHeader.jsx
+++ src/Banner/BannerHeader.jsx
@@ -1,28 +1,32 @@
 import React from 'react'
-import {Button } from 'flowbite-react';
+import { Button } from 'flowbite-react';
 import BannerImg from '../assets/slider-img.svg'
+import BannerBG from '../assets/banner-bg.svg'
 
 
 const BannerHeader = () => {
   return (
     <>
-      <div className='banner-part container mx-auto px-2 py-2.5 bg-[url("././")]'>
-        <div className='grid grid-cols-1 md:grid-cols-2'>
-          <div className=''>
-            <h4>Welcome to my world</h4>
-            <h1>Hi, I am  Tomal.M </h1>
-            <h2>I'm a UI/UX Designer</h2>
-            <p>I am always working on new thinking and trends.
-              The client's Happiness is everything to me.</p>
-              <button>Download CV</button>
-              <Button>Download CV</Button>
+      <div className="dark:bg-[url('./assets/banner-bg-dark.svg')] bg-[url('./assets/banner-bg.svg')]  bg-center bg-no-repeat bg-cover">
+        <div className="banner-part container mx-auto px-2 lg:py-[70px] py-3 ">
+          <div className='grid grid-cols-1 md:grid-cols-2'>
+            <div className='flex items-center'>
+              <div>
+                <h4 className='dark:text-white text-[22px] uppercase py-[3px]'>Welcome to my world</h4>
+                <h1 className='dark:text-white text-[32px] font-bold py-[3px]'>Hi, I am <span className='text-[#FF014F]'>Tomal.M </span></h1>
+                <h2 className='dark:text-white text-[32px] font-semibold py-[3px]'>I'm a <span className='text-[#46C5E8]'>UI/UX Designer</span></h2>
+                <p className='dark:text-[#f1f1f1] text-[#515151] text-[16px] font-regular py-[3px]'>I am always working on new thinking and trends. <br />
+                  The client's Happiness is everything to me.</p>
+                <Button className='my-3'>Download CV</Button>
+              </div>
+            </div>
+            <div className=''>
+              <img className='m-auto border dark:border-gray-600/60 border-gray-300 rounded-2xl' src={BannerImg} alt="" />
+            </div>
           </div>
-          <div className=''>
-            <img className='m-auto' src={BannerImg} alt="" />
-          </div>
-        </div>
-        {/* <img src="https://codesphere.com/img/llama2.webp" alt="" /> */}
-      </div >
+          <img src="" alt="" />
+        </div >
+      </div>
     </>
   )
 }
