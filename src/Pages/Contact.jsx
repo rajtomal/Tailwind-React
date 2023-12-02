2b12e2238dbf0cb8ef32f0a958ccd2a1f7ad3fff
Tomal <sj.tomalahmedraj@gmail.com>
2023-12-02 23:15:33 +0600

    contact

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import MainLayout from '../component/Layout/MainLayout'
+import Contact_details from '../component/contact_details/contact_details'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+      <MainLayout>
+        <Contact_details></Contact_details>
+      </MainLayout>
+    </>
   )
 }
 
