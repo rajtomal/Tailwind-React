10148c84ba6ad50d495d24cebc8fdbf94271e501
Tomal Ahmed Raj <100875045+rajtomal@users.noreply.github.com>
2023-12-14 18:06:20 +0600

    Update MainLayout.jsx

--- src/component/Layout/MainLayout.jsx
+++ src/component/Layout/MainLayout.jsx
@@ -6,7 +6,7 @@ const MainLayout = ({ children }) => {
     window.scrollTo(0, 0)
     return (
         <>
-            <div className="dark">
+            <div className="">
                 <div className='dark:bg-gray-800 bg-[#F7F9FC]'>
                     <NavBar></NavBar>
                         {children}
