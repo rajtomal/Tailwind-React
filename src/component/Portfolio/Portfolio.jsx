a53bf87a4aac7f350c7e27796f99530853226fa1
Tomal <sj.tomalahmedraj@gmail.com>
2023-12-22 22:13:31 +0600

    nnew

--- src/component/Portfolio/Portfolio.jsx
+++ src/component/Portfolio/Portfolio.jsx
@@ -1,5 +1,14 @@
 import React, { useEffect } from 'react'
 import projectData from '../Services/PortfolioData'
+import LightGallery from 'lightgallery/react';
+
+// import styles
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
+
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
 
 const Portfolio = () => {
     return (
@@ -13,7 +22,14 @@ const Portfolio = () => {
                         {projectData.map((item, index) => (
                             <div key={index} className='md:col-span-4 rounded-[10px] mx-3 md:m-0 p-3 bg-white border border-[#E2E2E2] dark:bg-[#2d3a4f] dark:border-[#566279]'>
                                 <div className='rounded-[10px] overflow-hidden relative'>
-                                    <img className='scale-100 hover:scale-125 transition-all duration-300' src={item.img} alt="" />
+                                    <LightGallery
+                                        speed={500}
+                                        plugins={[lgThumbnail, lgZoom]}
+                                    >
+                                        <a href={item.img}>
+                                            <img className='scale-100 hover:scale-125 transition-all duration-300' src={item.img} alt="" />
+                                        </a>
+                                    </LightGallery>
                                     <div className='absolute top-3 right-3 z-50'>
                                         {item.link ? <a className='h-[35px] w-[35px] bg-white drop-shadow-lg flex items-center justify-center rounded-[50%]' href={item.link} target='_blank'><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
                                             <path d="M14.4375 1.96875H19.0312V6.5625M18.0469 2.95312L13.125 7.875M11.1562 3.28125H5.25C4.72786 3.28125 4.2271 3.48867 3.85788 3.85788C3.48867 4.2271 3.28125 4.72786 3.28125 5.25V15.75C3.28125 16.2721 3.48867 16.7729 3.85788 17.1421C4.2271 17.5113 4.72786 17.7188 5.25 17.7188H15.75C16.2721 17.7188 16.7729 17.5113 17.1421 17.1421C17.5113 16.7729 17.7188 16.2721 17.7188 15.75V9.84375" stroke="#222831" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
